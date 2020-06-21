5349269b0eb1da6c4a64a5e23b9da6952d5446d7
Felipe Oliveira <felipeoliveira@MacBook-Pro.local>
2020-06-21 16:27:29 -0300

    configuração de estruturas e typescript

--- src/routes/v1/home/home.ts
+++ src/routes/v1/home/home.ts
@@ -1,11 +1,12 @@
 import express from 'express';
+import HomeService from '@services/home';
 
 var router = express.Router();
 
 router.get(
     '/',
     (req, res) => {
-        res.status(200).json("Home test")
+        res.status(200).json(HomeService.get())
     }
 );
 
---
+++ src/services/home.ts
@@ -0,0 +1,5 @@
+export default class HomeService {
+    public static get(){
+        return "Home test get"
+    }
+}
